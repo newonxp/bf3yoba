960100bb3f5b8d32c9bf3923a18e0ce244dcdf32
Kopylov Stephen <sk@pandra.ru>
2013-08-09 20:08:24 +0400

    some fixes

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
 function init(){
+    $(".poster").click(function (e){addGallery()})
     $(".chapter a").click(function(e) {
       // $(this).parent().find("div").first().slideToggle("fast");
-
         $(this).parent().find("div").first().slideToggle("fast", function(){
             if($(this).parent().find("div").first().is(":visible")){
-                $("html, body").animate({scrollTop: $(this).parent().offset().top});
+                $("html, body").animate({scrollTop: $(this).parent().offset().top-6});
             }
         });
         e.preventDefault();
@@ -17,8 +17,8 @@ $(document).ready(function() {
 $(function() {
 
     // Find all YouTube videos
-    var $allVideos = $("iframe[src^='http://www.youtube.com']"),
-
+    var $allVideos = $("iframe[src^='http://www.youtube.com']")
+    var $allPics= $(".poster")
     // The element that is fluid width
         $fluidEl = $(".inner_chapter").first();
 
@@ -33,6 +33,8 @@ $(function() {
             .removeAttr('width');
 
     });
+
+
 
     // When the window is resized
     // (You'll probably want to debounce this)
@@ -52,8 +54,35 @@ $(function() {
                 .height(newWidth * $el.data('aspectRatio'));
 
         });
-
+        $allPics.each(function() {
+            $(this).css("max-width",newWidth+"px")
+        });
         // Kick off one resize to fix all videos on page load
     }).resize();
 
 });
+
+function addGallery(){
+    var picArray=[]
+    for(var i=0;i<$(".poster").length;i++){
+        var obj={}
+        obj.url=$(".poster").eq(i).attr("src")
+        obj.caption = ""
+        picArray.push(obj)
+    }
+    var options = {
+            margin: 20,
+            getImageSource: function(obj){
+                return obj.url;
+            },
+            getImageCaption: function(obj){
+                return obj.caption;
+            }
+        },
+        instance = Code.PhotoSwipe.attach(
+            picArray,
+            options
+        );
+    instance.show(0);
+}
+
